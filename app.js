794e6b717a7949fa4bd363c733c073a9ef192b3b
Daniel Reich <daniel.reich@uni-ulm.de>
2014-06-16 16:41:58 +0200

    Search implemented partly

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ var express = require('express'),
 
 
 
+
 router.get('/api', function(req, res) {
     async.waterfall([
             function(callback) {
                 var arr = [];
                 var resarr = '';
-                https.get('https://api.twitch.tv/kraken/streams?limit=20', function(resp) {
-                    resp.setEncoding('utf8')
+                if (req.query.offset) {
+                    var offset = req.query.offset;
+                } else {
+                    var offset = 0;
+                }
+
+                https.get('https://api.twitch.tv/kraken/streams?limit=20&offset='+offset, function(resp) {
+                    resp.setEncoding('utf8');
                     resp.on('data', function(d) {
                         resarr += d;
                     });
@@ -24,7 +31,6 @@ router.get('/api', function(req, res) {
                         callback(null, arr);
                     });
                 });
-
 
             },
             function(names, done) {
@@ -61,6 +67,123 @@ router.get('/api', function(req, res) {
 
 });
 
+
+router.get('/api/search/:query', function(req, res) {
+    var search = req.params.query;
+    var stream = req.query.name;
+    if(search === "stream") {
+
+        async.waterfall([
+                function(callback) {
+                    var arr = [];
+                    var resarr = '';
+                    https.get('https://api.twitch.tv/kraken/streams/'+stream, function(resp) {
+                        resp.setEncoding('utf8')
+                        resp.on('data', function(d) {
+                            resarr += d;
+                        });
+                        resp.on('end', function() {
+                            resarr = JSON.parse(resarr);
+                            callback(null, resarr.stream);
+                        });
+                    });
+
+
+                },
+                function(names, done) {
+                    var bla = '';
+                    https.get('https://api.twitch.tv/api/channels/' + names.channel.name + '/access_token', function(resp) {
+                        resp.setEncoding('utf8')
+                        resp.on('data', function (d) {
+                            bla += d;
+                        });
+                        resp.on('end', function () {
+                            bla = JSON.parse(bla);
+                            bla = 'http://usher.justin.tv/api/channel/hls/' +names.channel.name + '.m3u8?token=' + bla.token + '&sig=' + bla.sig;
+                            names = [names, bla];
+                            done(null, names);
+                        });
+                    });
+                }
+            ],
+
+            function(err, result) {
+                if (err) throw err;
+                // var link = {};
+                // result.forEach(function(e) {//     var name = JSON.parse(e[1].token);
+                //     link[name.channel] = 'http://usher.justin.tv/api/channel/hls/' + name.channel + '.m3u8?token=' + e.token + '&sig=' + e.sig;
+                // });
+                console.log(result);
+                var sendarr = JSON.stringify(result);
+                res.json(sendarr);
+            });
+
+    } else if (search === 'game'){
+
+        async.waterfall([
+                function(callback) {
+                    var arr = [];
+                    var resarr = '';
+                    if (req.query.offset) {
+                        var offset = req.query.offset;
+                    } else {
+                        var offset = 0;
+                    }
+
+                    https.get('https://api.twitch.tv/kraken/search/streams?q='+stream+"limit=20", function(resp) {
+                        resp.setEncoding('utf8');
+                        resp.on('data', function(d) {
+                            resarr += d;
+                        });
+                        resp.on('end', function() {
+                            console.log("gamesearch called");
+                            resarr = JSON.parse(resarr);
+                            console.log(resarr);
+                            resarr.games.forEach(function(e) {
+                                arr.push(e);
+                            });
+                            callback(null, arr);
+                        });
+                    });
+
+                },
+                function(names, done) {
+                    async.each(names, function(e, callback) {
+                        var bla = '';
+                        https.get('https://api.twitch.tv/api/channels/' + e.channel.name + '/access_token', function(resp) {
+                            resp.setEncoding('utf8')
+                            resp.on('data', function(d) {
+                                bla += d;
+                            });
+                            resp.on('end', function() {
+                                bla = JSON.parse(bla);
+                                bla = 'http://usher.justin.tv/api/channel/hls/' + e.channel.name + '.m3u8?token=' + bla.token + '&sig=' + bla.sig;
+                                names[names.indexOf(e)] = [e, bla];
+                                callback(null);
+                            });
+                        });
+                    }, function(err) {
+                        if (err) throw err;
+                        done(null, names);
+                    });
+                }
+            ],
+
+            function(err, result) {
+                if (err) throw err;
+                // var link = {};
+                // result.forEach(function(e) {//     var name = JSON.parse(e[1].token);
+                //     link[name.channel] = 'http://usher.justin.tv/api/channel/hls/' + name.channel + '.m3u8?token=' + e.token + '&sig=' + e.sig;
+                // });
+                var sendarr = JSON.stringify(result);
+                res.json(sendarr);
+            });
+
+
+
+    }
+
+});
 
 app.use(express.static(__dirname + "/public"));
 app.use('/bower_components', express.static(__dirname + "/bower_components"));
--- public/index.js
+++ public/index.js
@@ -1,43 +1,49 @@
-$(document).ready(function() {
-    $.get('/api', function(data) {
+$(document).ready(function () {
+    $.get('/api', function (data) {
         $('#loading').remove();
 
         var newdata = JSON.parse(data);
-        $.each(newdata, function(i, e) {
+        $.each(newdata, function (i, e) {
             $('#container').append('<div class="col-md-15"><div class="thumbnail stream"><img width="100%" src="' + e[0].preview.medium + '" alt="..."><div class="overlay"><div class="caption text-center"><a href="' + e[1].replace(/"/g, '&#34') + '">' + e[0].channel.display_name + '</a>' +
                 '<p class = "game-name">' + e[0].channel.game + '</p> <span>' + e[0].viewers + ' Viewer </span></div></div></div></div>');
         });
 
-    }).done(function() {
-        $('#container').append('<button id="load" type="button" class="btn btn-default center-block button-dark-l">Dropup</button>')
-        $("div.stream").on('click', function() {
+    }).done(function () {
+
+        $('#container').append('<button id="load" type="button" class="btn btn-default center-block button-dark-l">Load More</button>');
+        $("div.stream").on('click', function () {
             window.location = $(this).find("a").attr("href");
             return false;
         });
 
+        $("#load").on('click', function () {
+            loadMore();
+        });
     });
 
 
+    $('#searchform').submit(function (e) {
+
+        search(e.target[1].value, e.target[0].value);
+        e.preventDefault();
+    });
 });
 
 
 function loadMore() {
     var offset = $('.stream').length;
-    $('#load').prepend('<div id="loading"><span>Loading</span><span class="l-1"></span><span class="l-2"></span><span class="l-3"></span><span class="l-4"></span><span class="l-5"></span><span class="l-6"></span></div>');
 
-
-    $.get('/api/', function(data) {
+    $.get('/api?offset=' + offset, function (data) {
         $('#loading').remove();
 
         var newdata = JSON.parse(data);
-        $.each(newdata, function(i, e) {
-            $('#container').append('<div class="col-md-15"><div class="thumbnail stream"><img width="100%" src="' + e[0].preview.medium + '" alt="..."><div class="overlay"><div class="caption text-center"><a href="' + e[1].replace(/"/g, '&#34') + '">' + e[0].channel.display_name + '</a>' +
+        $.each(newdata, function (i, e) {
+            $('#load').before('<div class="col-md-15"><div class="thumbnail stream"><img width="100%" src="' + e[0].preview.medium + '" alt="..."><div class="overlay"><div class="caption text-center"><a href="' + e[1].replace(/"/g, '&#34') + '">' + e[0].channel.display_name + '</a>' +
                 '<p class = "game-name">' + e[0].channel.game + '</p> <span>' + e[0].viewers + ' Viewer </span></div></div></div></div>');
         });
 
-    }).done(function() {
-        $('#container').append('<button id="load" type="button" class="btn btn-default center-block button-dark-l">Dropup</button>')
-        $("div.stream").on('click', function() {
+    }).done(function () {
+        $("div.stream").on('click', function () {
             window.location = $(this).find("a").attr("href");
             return false;
         });
@@ -46,3 +52,48 @@ function loadMore() {
 
 
 }
+
+function search(param, val) {
+    if (param === 'Stream') {
+        $.get('/api/search/stream?name=' + val, function (data) {
+
+            $('#loading').remove();
+            $('#container').empty();
+            var newdata = JSON.parse(data);
+            console.log(newdata);
+                $('#container').append('<div class="col-md-15"><div class="thumbnail stream"><img width="100%" src="' + newdata[0].preview.medium + '" alt="..."><div class="overlay"><div class="caption text-center"><a href="' + newdata[1].replace(/"/g, '&#34') + '">' + newdata[0].channel.display_name + '</a>' +
+                    '<p class = "game-name">' + newdata[0].channel.game + '</p> <span>' + newdata[0].viewers + ' Viewer </span></div></div></div></div>');
+
+        }).done(function () {
+            $("div.stream").on('click', function () {
+                window.location = $(this).find("a").attr("href");
+                return false;
+            });
+
+        });
+
+    } else if (param === 'Game') {
+        console.log("GAMESEARCH");
+        $.get('/api/search/game?name=' + val, function (data) {
+
+            $('#loading').remove();
+            $('#container').empty();
+            var newdata = JSON.parse(data);
+            console.log(newdata);
+            $('#container').append('<div class="col-md-15"><div class="thumbnail stream"><img width="100%" src="' + newdata[0].preview.medium + '" alt="..."><div class="overlay"><div class="caption text-center"><a href="' + newdata[1].replace(/"/g, '&#34') + '">' + newdata[0].channel.display_name + '</a>' +
+                '<p class = "game-name">' + newdata[0].channel.game + '</p> <span>' + newdata[0].viewers + ' Viewer </span></div></div></div></div>');
+
+        }).done(function () {
+            $("div.stream").on('click', function () {
+                window.location = $(this).find("a").attr("href");
+                return false;
+            });
+
+        });
+
+
+    }
+
+
+}
+
